c44a81c6b06c8e745273af6a9f02f043da1ad548
Roya Hossaini <royarahyab53@gmail.com>
2025-07-16 09:36:01 +0430

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
-
 Route::get('/contact', [ContactController::class, 'show'])->name('contact.show');
 Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
 
 Route::get('/thankyou', function () {
     return view('thankyou');
 })->name('thankyou');
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
